ef4bf70047504c6d287dc8b5c7c3ebd3e45400d7
Syed Husain <sahusain1984@gmail.com>
2025-05-01 16:42:30 -0400

    Update script1.py

--- Sourcecode/script1.py
+++ Sourcecode/script1.py
@@ -28,4 +28,4 @@ def trigger_workflow(workflow_Name,parameter1,parameter2):
       responsevalue=requests.post(f"https://api.github.com/repos/{OWNER}/{REPO}/dispatches",json=data,headers=headers)
       print(responsevalue.content)
 
-trigger_workflow(workflowname,parameter1,parameter2)
+trigger_workflow(workflow_Name,parameter1,parameter2)
